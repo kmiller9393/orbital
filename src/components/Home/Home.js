482aef6ab50aa81bf9a81ee8c60f02b7b16595a9
Kurt Miller <kmiller9393@gmail.com>
2018-09-08 19:37:41 -0600

    Add Home Component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import './Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1 className="App-title">Orbital</h1>
+          <h1 className="App-title">Welcome back, Brandon</h1>
+        </header>
+        <button className="home-button">
+          <NavLink className="review-link" to="/review">
+            Review
+          </NavLink>
+        </button>
+      </div>
+    );
+  }
+}
