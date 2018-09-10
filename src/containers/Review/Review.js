aee71a25c1071cf00992f94e4e96facad2c0ebc5
Kurt Miller <kmiller9393@gmail.com>
2018-09-09 20:23:29 -0600

    Change api fetch from rover API to hubble glossary API

--- src/containers/Review/Review.js
+++ src/containers/Review/Review.js
@@ -2,26 +2,25 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink, withRouter } from 'react-router-dom';
 import { populatePage } from '../../actions';
-import key from '../../apiKey';
 import './Review.css';
 
 class Review extends Component {
   setReviewInfo = async () => {
     const response = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=${key}`
+      'http://hubblesite.org/api/v3/glossary?page=all'
     );
-    const result = await response.json();
-    this.props.getSpaceData(result);
+    const fetchedFacts = await response.json();
+    this.props.getSpaceData(fetchedFacts);
   };
 
   render() {
     return (
       <div>
         <h2>Orbital</h2>
-        <h2>Review Topics</h2>
+        <h2>Review Sections</h2>
         <button className="review-button" onClick={this.setReviewInfo}>
-          <NavLink className="mars-link" to="/mars-review">
-            Mars
+          <NavLink className="review-link" to="/one-review">
+            Week 1
           </NavLink>
         </button>
       </div>
@@ -30,7 +29,7 @@ class Review extends Component {
 }
 
 export const mapDispatchToProps = dispatch => ({
-  getSpaceData: loadingData => dispatch(populatePage(loadingData))
+  getSpaceData: glossary => dispatch(populatePage(glossary))
 });
 
 export default withRouter(
