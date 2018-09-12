d438cded4fdcb695c5cec1e58e80df41ecf24739
Kurt Miller <kmiller9393@gmail.com>
2018-09-12 10:56:47 -0600

    Add mapDispatchToProps to TriviaContainer so each TriviaCard it returns has access to props

--- src/containers/TriviaContainer/TriviaContainer.js
+++ src/containers/TriviaContainer/TriviaContainer.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
 import TriviaCard from '../TriviaCard/TriviaCard';
-import { withRouter } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import orbital from '../../images/orbital-header.svg';
 import './TriviaContainer.css';
 
 class TriviaContainer extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
   render() {
+    const answers = this.props.answers.map((answer, index) => {
+      return (
+        <TriviaCard
+          answer={Object.keys(answer)[0]}
+          question={Object.values(answer)[0]}
+          key={index}
+        />
+      );
+    });
+
     return (
       <div>
-        <img className="trivia-container-image" src={orbital} alt="Orbital" />
+        <NavLink to="/">
+          <img className="trivia-container-image" src={orbital} alt="Orbital" />
+        </NavLink>
         <h2 className="trivia-title">Week 1 Trivia</h2>
-        <TriviaCard />
+        <div className="answers-container">{answers}</div>
       </div>
     );
   }
 }
 
-export default TriviaContainer;
+export const mapStateToProps = state => ({
+  answers: state.triviaAnswers
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(TriviaContainer);
