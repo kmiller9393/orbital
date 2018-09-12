a5b437c749e6ccb86936558500292dc1a7a2f03d
Kurt Miller <kmiller9393@gmail.com>
2018-09-11 20:55:03 -0600

    Update helper file

--- src/utils/helper.js
+++
@@ -1,2 +0,0 @@
-//clean hubble glossary data so definition values' first sentence are returned - answer on flashcards
-//OR only clean first sentence for quiz questions && keep entire definition for review.
