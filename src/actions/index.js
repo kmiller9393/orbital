902cca7d425ba2eae9dd61ded3614fa9cb4110f0
Kurt Miller <kmiller9393@gmail.com>
2018-09-15 17:37:13 -0600

    Add addScore action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,14 @@
-export const populatePage = glossary => ({
-  type: 'POPULATE_PAGE',
+export const addGlossary = glossary => ({
+  type: 'ADD_GLOSSARY',
   glossary
 });
 
-export const populateTrivia = triviaSet => ({
-  type: 'POPULATE_TRIVIA',
+export const addTriviaSet = triviaSet => ({
+  type: 'ADD_TRIVIA_SET',
   triviaSet
+});
+
+export const addScore = score => ({
+  type: 'ADD_SCORE',
+  score
 });
