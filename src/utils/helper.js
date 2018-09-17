6fc366bab36f6dbb948b3cd72c16be19fc5ae4a5
Kurt Miller <kmiller9393@gmail.com>
2018-09-16 21:35:12 -0600

    Add methods that clean review data based on weekly selection to helper file

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -15,9 +15,19 @@ export const addTriviaOne = triviaSet => {
   return firstTriviaSet;
 };
 
+export const addReviewOne = reviewSet => {
+  const firstReviewSet = reviewSet.slice(0, 9);
+  return firstReviewSet;
+};
+
 export const addTriviaTwo = triviaSet => {
   const secondTriviaSet = triviaSet.slice(9, 18);
   return secondTriviaSet;
+};
+
+export const addReviewTwo = reviewSet => {
+  const secondReviewSet = reviewSet.slice(9, 18);
+  return secondReviewSet;
 };
 
 export const addTriviaThree = triviaSet => {
@@ -25,12 +35,27 @@ export const addTriviaThree = triviaSet => {
   return thirdTriviaSet;
 };
 
+export const addReviewThree = reviewSet => {
+  const thirdReviewSet = reviewSet.slice(18, 27);
+  return thirdReviewSet;
+};
+
 export const addTriviaFour = triviaSet => {
   const fourthTriviaSet = triviaSet.slice(27, 36);
   return fourthTriviaSet;
 };
 
+export const addReviewFour = reviewSet => {
+  const fourthReviewSet = reviewSet.slice(27, 37);
+  return fourthReviewSet;
+};
+
 export const addTriviaFive = triviaSet => {
   const fifthTriviaSet = triviaSet.slice(36, 45);
   return fifthTriviaSet;
+};
+
+export const addReviewFive = reviewSet => {
+  const fifthReviewSet = reviewSet.slice(37, 47);
+  return fifthReviewSet;
 };
