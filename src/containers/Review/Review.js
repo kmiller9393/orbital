a08fef856bb79dfad59af2b624d7d36ef0267cc1
Kurt Miller <kmiller9393@gmail.com>
2018-09-16 21:33:43 -0600

    Remove setReviewInfo method from Reivew component

--- src/containers/Review/Review.js
+++ src/containers/Review/Review.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import { populateFacts } from '../../thunks';
-import PropTypes from 'prop-types';
 import orbital from '../../images/orbital-header.svg';
 import './Review.css';
 
-class Review extends Component {
-  setReviewInfo = async () => {
-    await this.props.populateFacts();
-  };
-
+export default class Review extends Component {
   render() {
     return (
       <div className="review-container">
@@ -19,27 +12,27 @@ class Review extends Component {
         </NavLink>
         <h2 className="review-header">Weekly Reviews</h2>
         <div className="review-button-container">
-          <button className="review-button" onClick={this.setReviewInfo}>
+          <button className="review-button">
             <NavLink className="review-page-link" to="/review-one">
               Week 1
             </NavLink>
           </button>
-          <button className="review-button" onClick={this.setReviewInfo}>
+          <button className="review-button">
             <NavLink className="review-page-link" to="/review-two">
               Week 2
             </NavLink>
           </button>
-          <button className="review-button" onClick={this.setReviewInfo}>
+          <button className="review-button">
             <NavLink className="review-page-link" to="/review-three">
               Week 3
             </NavLink>
           </button>
-          <button className="review-button" onClick={this.setReviewInfo}>
+          <button className="review-button">
             <NavLink className="review-page-link" to="/review-four">
               Week 4
             </NavLink>
           </button>
-          <button className="review-button" onClick={this.setReviewInfo}>
+          <button className="review-button">
             <NavLink className="review-page-link" to="/review-five">
               Week 5
             </NavLink>
@@ -49,17 +42,3 @@ class Review extends Component {
     );
   }
 }
-
-export const mapDispatchToProps = dispatch => ({
-  populateFacts: () => dispatch(populateFacts())
-});
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(Review);
-
-const { func } = PropTypes;
-Review.propTypes = {
-  populateFacts: func
-};
