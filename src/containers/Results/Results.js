8762b2c018d0f41445194ba28646d054868119d6
Kurt Miller <kmiller9393@gmail.com>
2018-09-16 20:02:32 -0600

    Add Results container that displays score to user

--- src/containers/Results/Results.js
+++ src/containers/Results/Results.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { resetScore } from '../../actions';
+import orbital from '../../images/orbital-header.svg';
+import './Results.css';
 
 class Review extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
   render() {
-    return <div />;
+    const { score, resetScore } = this.props;
+
+    return (
+      <div>
+        <NavLink to="/" onClick={() => resetScore()}>
+          <img className="results-image" src={orbital} alt="Orbital" />
+        </NavLink>
+        <div className="score-container">
+          <h1 className="results-header">Your Results</h1>
+          <p className="score">
+            Score: <span>{((score / 9) * 100).toFixed(2)}%</span>
+          </p>
+        </div>
+      </div>
+    );
   }
 }
 
@@ -15,7 +29,11 @@ export const mapStateToProps = state => ({
   score: state.score
 });
 
+export const mapDispatchToProps = dispatch => ({
+  resetScore: () => dispatch(resetScore())
+});
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(Review);
