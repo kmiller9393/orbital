99c4fa9460c9072b161e0701d4b2343374356c98
Kurt Miller <kmiller9393@gmail.com>
2018-09-17 12:38:39 -0600

    Move thunks into their own individual directories

--- src/thunks/populateTriviaFacts.js
+++ src/thunks/populateTriviaFacts.js
@@ -1,19 +1,6 @@
-import { addGlossary } from '../actions';
 import { addTriviaSet } from '../actions';
 import { fetchSpaceFacts } from '../utils/apiCalls';
 import { cleanFacts } from '../utils/helper';
-
-export const populateReview = () => {
-  return async dispatch => {
-    try {
-      const facts = await fetchSpaceFacts();
-      const factsWithId = facts.map((fact, i) => ({ ...fact, id: i }));
-      dispatch(addGlossary(factsWithId));
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  };
-};
 
 export const populateTriviaFacts = () => {
   return async dispatch => {
