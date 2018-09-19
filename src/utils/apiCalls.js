b528552cbbc93e56ee25b5f58cdb8526a3a41b5f
Kurt Miller <kmiller9393@gmail.com>
2018-09-19 09:30:06 -0600

    update fetch url

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,5 +1,5 @@
 export const fetchSpaceFacts = async () => {
-  const url = 'http://hubblesite.org/api/v3/glossary?page=all';
+  const url = 'https://hubblesite.org/api/v3/glossary?page=all';
   try {
     const response = await fetch(url);
     const fetchedFacts = await response.json();
