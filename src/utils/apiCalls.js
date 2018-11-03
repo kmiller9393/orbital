2dbe3122c16921d88970cd429303fa0a729627dd
Kurt Miller <kmiller9393@gmail.com>
2018-11-03 11:24:48 -0600

    Update url within fetchSpaceFacts so there is no CORS error in the browser

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,5 +1,6 @@
 export const fetchSpaceFacts = async () => {
-  const url = 'http://hubblesite.org/api/v3/glossary?page=all';
+  const url =
+    'https://cors-anywhere.herokuapp.com/http://hubblesite.org/api/v3/glossary?page=all';
   try {
     const response = await fetch(url);
     const fetchedFacts = await response.json();
