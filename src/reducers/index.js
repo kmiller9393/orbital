53074b1edd37eaeed35492e3af2ec2808047a13d
Kurt Miller <kmiller9393@gmail.com>
2018-12-03 22:49:03 -0700

    Add currentEventsReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { populateReducer } from './populateReducer';
 import { triviaReducer } from './triviaReducer';
+import { currentEventsReducer } from './currentEventsReducer';
 import { scoreReducer } from './scoreReducer';
 
 const rootReducer = combineReducers({
   glossary: populateReducer,
   triviaAnswers: triviaReducer,
+  spaceEvents: currentEventsReducer,
   score: scoreReducer
 });
 
