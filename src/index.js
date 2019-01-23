d18b320cb9a13a18f7ab6a9b508d80d149b79239
Kurt Miller <kmiller9393@gmail.com>
2019-01-22 22:46:43 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
@@ -9,7 +10,10 @@ import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './reducers';
 import './index.css';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
