0aeb6be0f81fcd06ac1179875c8195f4ad123383
Kurt Miller <kmiller9393@gmail.com>
2019-01-22 22:38:50 -0700

    Remove composeWithDevTools within index.js to solve cross-browser compatibility issues

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
@@ -10,10 +9,7 @@ import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './reducers';
 import './index.css';
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
